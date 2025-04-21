e188a2e129d530fe4b0d5759c5ce531f68080e01
pholenk <pholenkadi17@gmail.com>
2025-04-21 10:51:34 +0700

    initial project setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,16 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  plugins: ['jest'],
+  parser: '@typescript-eslint/parser',
+  overrides: [
+    {
+      // Test files only
+      files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+      env: {
+        'jest/globals': true,
+      },
+      extends: ['plugin:testing-library/react', 'plugin:jest/recommended'],
+    },
+  ],
 };
