050409ccad1a8142c1ac4ae7ada7f41c87e35cd2
pholenk <pholenkadi17@gmail.com>
2025-04-22 02:53:36 +0700

    add cell style and header table

--- src/ui/organisms/Table/Table.tsx
+++ src/ui/organisms/Table/Table.tsx
@@ -5,17 +5,32 @@ import {RowTable, RowTableProps} from '@molecules';
 
 export interface TableProps {
   data: RowTableProps['cells'][];
+  header?: RowTableProps['cells'];
 }
 
 export const Table: FC<TableProps> = props => {
   const styles = TableStyles;
 
-  const RenderRow = useCallback(({item}: {item: RowTableProps['cells']}) => {
-    return <RowTable cells={item} />;
-  }, []);
+  const RenderHeader = useCallback(() => {
+    const header = [...(props?.header ?? [])];
+
+    if (header.length > 0) {
+      return <RowTable cells={header} cellStyle={styles.header} />;
+    }
+
+    return null;
+  }, [props?.header, styles.header]);
+
+  const RenderRow = useCallback(
+    ({item}: {item: RowTableProps['cells']}) => {
+      return <RowTable cells={item} cellStyle={styles.cellItem} />;
+    },
+    [styles.cellItem],
+  );
 
   return (
     <FlatList
+      ListHeaderComponent={RenderHeader}
       style={styles.container}
       data={props.data}
       renderItem={RenderRow}
--- src/ui/organisms/Table/TableStyles.tsx
+++ src/ui/organisms/Table/TableStyles.tsx
@@ -5,4 +5,14 @@ export const TableStyles = StyleSheet.create({
     flex: 1,
     width: '100%',
   },
+  header: {
+    borderWidth: 1,
+    backgroundColor: '#e9e9e9',
+  },
+  cellItem: {
+    borderWidth: 1,
+    backgroundColor: '#ffffff',
+    padding: 8,
+    flexWrap: 'wrap',
+  },
 });
