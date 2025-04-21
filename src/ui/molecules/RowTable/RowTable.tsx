a77495c259c1338611513c44d4a08de03a4cbfb6
pholenk <pholenkadi17@gmail.com>
2025-04-22 02:52:51 +0700

    add onCellLayout to calculate min height of cell

--- src/ui/molecules/RowTable/RowTable.tsx
+++ src/ui/molecules/RowTable/RowTable.tsx
@@ -1,6 +1,6 @@
-import React, {FC} from 'react';
+import React, {FC, useState} from 'react';
 import {RowTableStyles} from './RowTableStyles';
-import {View} from 'react-native';
+import {LayoutChangeEvent, View} from 'react-native';
 import {TableCell, TableCellProps} from '@atoms';
 
 export interface RowTableProps {
@@ -10,16 +10,22 @@ export interface RowTableProps {
 
 export const RowTable: FC<RowTableProps> = props => {
   const styles = RowTableStyles;
+  const [minHeight, setMinHeight] = useState(0);
 
   const keyEtractor = (length: number, index = 0) =>
     `${length / (length - 1) - index}`;
 
+  const onCellLayout = (event: LayoutChangeEvent) => {
+    const {height} = event.nativeEvent.layout;
+    setMinHeight(height);
+  };
+
   return (
-    <View style={styles.container}>
+    <View style={styles.container} onLayout={onCellLayout}>
       {props.cells.map((cell, index) => (
         <TableCell
           key={keyEtractor(props.cells.length ?? 0, index)}
-          style={props.cellStyle}>
+          style={[props.cellStyle, {minHeight: minHeight}]}>
           {cell}
         </TableCell>
       ))}
