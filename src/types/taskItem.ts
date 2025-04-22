682cd85ba1d2a364acb270a3efbdde4ddda075b0
pholenk <pholenkadi17@gmail.com>
2025-04-22 14:04:21 +0700

    create task item type

--- src/types/taskItem.ts
+++ src/types/taskItem.ts
@@ -4,8 +4,10 @@ export type TaskItem = {
   type: TaskType;
   fieldName: string;
   name: string;
+  crop: string;
   dueBy: string;
   dueDate: string;
+  lastAction: string;
   instruction: string;
   attachment: string;
 };
